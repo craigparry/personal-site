b860edc6a52b88f91f583b039a4569e58ede347c
Craig Parry <craigparry@Craigs-MacBook-Pro.local>
2024-01-17 18:18:04 -0700

    added pdf for resume

--- personal-site/src/app/components/Footer.tsx
+++ personal-site/src/app/components/Footer.tsx
@@ -4,16 +4,12 @@ import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import { Box } from "@mui/material";
 import { useTheme } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { useRouter } from 'next/navigation'
-
 
 export default function Footer() {
     const theme = useTheme();
-    const router = useRouter()
 
     return (
         <Box
@@ -26,34 +22,26 @@ export default function Footer() {
             }}
             component="footer"
         >
-            <Grid container spacing={2}>
-                <Grid item xs={9} />
-                <Grid xs={3} container direction="column" alignItems="flex-end" >
-                    <a target="_blank" href="https://www.linkedin.com/in/craig-parry-599a70144/" rel="noopener noreferrer">
-                        <IconButton onClick={() => { console.log('linkedin') }} size='small' >
-                            <LinkedInIcon sx={{ marginTop: "1rem", color: theme.palette.primary.contrastText }} />
-                        </IconButton>
-                    </a>
-                </Grid>
-                <Grid item xs={9} />
-                <Grid xs={3} container direction="column" alignItems="flex-end" >
-                    <a target="_blank" href="https://github.com/craigparry" rel="noopener noreferrer">
-                        <IconButton onClick={() => { console.log('github') }} size='small' >
-                            <GitHubIcon sx={{ marginTop: "1rem", color: theme.palette.primary.contrastText }} />
-                        </IconButton>
-                    </a>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="body2" color='primary.contrastText' align="center" >
-                        {"Copyright © "}
-                        <Link color="inherit" href="https://google.com/" rel="noopener noreferrer">
-                            Your Website
-                        </Link>{" "}
-                        {new Date().getFullYear()}
-                        {"."}
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Box>
+            <Box display="flex" justifyContent="flex-end">
+                <a target="_blank" href="https://github.com/craigparry" rel="noopener noreferrer">
+                    <IconButton onClick={() => { console.log('github') }} size='small' >
+                        <GitHubIcon sx={{ color: theme.palette.primary.contrastText }} />
+                    </IconButton>
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/craig-parry-599a70144/" rel="noopener noreferrer">
+                    <IconButton onClick={() => { console.log('linkedin') }} size='small' >
+                        <LinkedInIcon sx={{ color: theme.palette.primary.contrastText }} />
+                    </IconButton>
+                </a>
+            </Box>
+            <Typography variant="body2" color='primary.contrastText' align="center" >
+                {"Copyright © "}
+                <Link color="inherit" href="https://google.com/" rel="noopener noreferrer">
+                    Your Website
+                </Link>{" "}
+                {new Date().getFullYear()}
+                {"."}
+            </Typography>
+        </Box >
     );
 }
